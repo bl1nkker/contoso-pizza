c9fe8bfe76eb177cb7a192c6b3c802d1b6088e23
bl1nkker <nurovich14@gmail.com>
2022-01-11 22:32:37 +0600

    feat: added db + migrations

---
+++ Data/PizzaContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data;
+
+public class PizzaContext : DbContext
+{
+// This allows external code to pass in the configuration, so the same DbContext can be shared between test and production code and even used with different providers.
+    public PizzaContext (DbContextOptions<PizzaContext> options)
+        : base(options)
+    {
+    }
+
+    // The DbSet<T> properties correspond to tables to be created in the database.
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+    public DbSet<Topping> Toppings => Set<Topping>();
+    public DbSet<Sauce> Sauces => Set<Sauce>();
+
+    public DbSet<Gay> Gays => Set<Gay>();
+}
---
+++ Models/Gay.cs
@@ -0,0 +1,10 @@
+namespace ContosoPizza.Models;
+
+public class Gay
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int DickLength { get; set; }
+}
